8d115c30c973dde41d748a44322f314b5a4bad96
Saturnin Wanonkou <saturninwanonkou17@gmail.com>
2025-10-15 03:29:44 +0200

    fix

---
+++ sqrt_transformer.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity sqrt_transformer is
+    generic (
+        nb_bits : integer := 32
+    );
+    port (
+        iX, iZ, iV        : in   std_logic_vector(2*nb_bits-1 downto 0); 
+        oX, oZ, oV        : out  std_logic_vector(2*nb_bits-1 downto 0) 
+    );
+end entity sqrt_transformer;
+architecture arch of sqrt_transformer is
+begin
+	oZ <= '0'&reg_Z(2*nb_bits-1 downto 1) when unsigned(iX) < (unsigned(iZ) + unsigned(iV))
+			std_logic_vector(unsigned('0'&iZ(2*nb_bits-1 downto 1))+unsigned(iV)) when others;
+	oX <= iX when unsigned(iX) < (unsigned(iZ) + unsigned(iV))
+			std_logic_vector(unsigned(iX) - unsigned(iZ) - unsigned(iV)) when others;
+	oV <= "00"&iV(2*nb_bits-1 downto 2);
+end arch;
